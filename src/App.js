7ece2fea6d7a91dac068ee276a3770c28441975a
akshay-anil22 <92985589+akshay-anil22@users.noreply.github.com>
2024-08-05 20:24:57 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
-function App() {
+//d175f8c8
+const API_URL = "http://www.omdbapi.com?apikey=d175f8c8";
+
+const movie1 = {
+  Title: "Amazing Spiderman Syndrome",
+  Year: "2012",
+  imdbID: "tt2586634",
+  Type: "movie",
+  Poster: "N/A",
+};
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [totalresult, setTotalResult]= useState(0);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+    setMovies(data.Search);
+    setTotalResult(data.totalResults);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          placeholder="search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+        <h3>Total Results : {totalresult} </h3>
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((val) => (
+            <MovieCard movie={val} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2> No movies found</h2>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Trial.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Trial = () => {
+
+
+  return(
+    <>
+      <h1>
+        hii
+      </h1>
+    </>
+  )
+}
+
+export default Trial;
